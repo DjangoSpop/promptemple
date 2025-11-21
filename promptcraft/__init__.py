02e084cf48e9bd25d431d49e5cd8218fcf60be9d
Ahmed el bahi <81105228+bahi-ops@users.noreply.github.com>
2025-11-21 22:36:57 +0200

    Fix: Remove Celery import for Heroku deployment

--- promptcraft/__init__.py
+++ promptcraft/__init__.py
@@ -1,8 +1,6 @@
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-try:
-    from .celery import app as celery_app
-    __all__ = ('celery_app',)
-except ImportError:
-    # Celery not installed - running in minimal mode
-    __all__ = ()
+
+# Celery removed for Heroku deployment (no background tasks)
+# from .celery import app as celery_app
+# __all__ = ('celery_app',)
